9708bedede34211d6bccfb367c25e422cb7a511a
mbmosman <mbMosman@users.noreply.github.com>
2021-03-19 17:09:59 -0500

    split assignments

---
+++ assignment/scripts/object-practice.js
@@ -0,0 +1,45 @@
+console.log('***** Object Practice *****')
+
+// Add the required code to complete the tasks below
+
+/* 1. Create an object literal to represent you!
+  - Give the object a 'firstName & (separate) 'lastName' property
+  - Give it a boolean 'hasSiblings' property
+  - Give it  a numeric 'shoeCount' property
+  - Also give it an array 'favThreeFoods'
+*/
+const me = {
+  // TODO - add properties here
+};
+console.log('A little about me:', me);
+
+/* 2. Accessing object properties.
+  - Create a variable called fullName 
+  - Use the firstName & lastName properties of the object you 
+    created above to set its value. 
+  - Console.log fullName
+*/
+
+
+
+/* 3. Nested arrays
+  - Console.log your first favorite food
+  - Console.log your last favorite food 
+*/
+
+
+
+/* 4. Change a property of an existing object.
+  - Console.log your current shoe count.
+  - Guess what?! You just got a new pair of shoes!!!
+  - Set your object's shoeCount to be what it was plus 1
+  - Console.log your updated shoe count. 
+*/
+
+
+/* 5. Add a new property to an existing object.
+  - Add a 'favoriteColor' to your existing object
+  - Do this the same way you updated the variable above
+  - Cool huh? It works, even though it wasn't there before
+  - Console.log your object
+*/
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Tier 1 Week 5 - Part 1
+
+This week, we'll be exploring objects by creating an application to help maintain a record collection. We will be creating objects using `object literal` syntax.
+
+## Topics Covered
+
+- objects
+- HTML & CSS
+
+## Assignment - Function Practice
+
+**Reminder**: Make sure to answer this week's Slack discussion question!
+
+
+Complete the `object-practice.js` file. 
+
+Follow the instructions in the comments to add the required code. (Please Do not remove the instruction comments!)
+
+
+## Assignment Submission
+Check in your repo, then turn in your work via the Prime Academy Assignment Application at http://primeacademy.io, as usual and don't hesitate to hit up the Slack channel as needed!
